49a98c46aeaeab21e6de292bce41a02efa5c7b05
Willa C. Riggins <willacassriggins@gmail.com>
2017-02-23 22:28:40 -0500

    Fixed api

--- cards/views.py
+++ cards/views.py
@@ -8,7 +8,7 @@ import datetime
 def scan_card(request, scanned_card):
     now = datetime.datetime.now()
     c = Card.objects.filter(card=scanned_card).first()
-    if c is None:
+    if c is not None:
         res = {'now': now, 'status': c.member.status}
     else:
         res = {'now': now, 'status': 0}
